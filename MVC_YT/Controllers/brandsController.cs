e7bc1122352db9b571f7bc7ed06bf3a7a48a3c26
Jay Chaniyara <Jay.Chaniyara@civica2.com>
2022-12-20 23:11:49 +0530

    first commit

---
+++ MVC_YT/Controllers/brandsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_YT.Models;
+
+namespace MVC_YT.Controllers
+{
+    public class brandsController : Controller
+    {
+        private MVC_YTEntities db = new MVC_YTEntities();
+
+        // GET: brands
+        public ActionResult Index()
+        {
+            return View(db.brands.ToList());
+        }
+
+        // GET: brands/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            brand brand = db.brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // GET: brands/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: brands/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,brand1")] brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.brands.Add(brand);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(brand);
+        }
+
+        // GET: brands/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            brand brand = db.brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: brands/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,brand1")] brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(brand).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(brand);
+        }
+
+        // GET: brands/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            brand brand = db.brands.Find(id);
+            if (brand == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brand);
+        }
+
+        // POST: brands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            brand brand = db.brands.Find(id);
+            db.brands.Remove(brand);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC_YT/Controllers/productsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_YT.Models;
+
+namespace MVC_YT.Controllers
+{
+    public class productsController : Controller
+    {
+        private MVC_YTEntities db = new MVC_YTEntities();
+
+        // GET: products
+        public ActionResult Index()
+        {
+            var products = db.products.Include(p => p.brand).Include(p => p.category);
+            return View(products.ToList());
+        }
+
+        // GET: products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: products/Create
+        public ActionResult Create()
+        {
+            ViewBag.brand_id = new SelectList(db.brands, "id", "brand1");
+            ViewBag.category_id = new SelectList(db.categories, "id", "category1");
+            return View();
+        }
+
+        // POST: products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,productName,category_id,brand_id,quantity,price")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.brand_id = new SelectList(db.brands, "id", "brand1", product.brand_id);
+            ViewBag.category_id = new SelectList(db.categories, "id", "category1", product.category_id);
+            return View(product);
+        }
+
+        // GET: products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.brand_id = new SelectList(db.brands, "id", "brand1", product.brand_id);
+            ViewBag.category_id = new SelectList(db.categories, "id", "category1", product.category_id);
+            return View(product);
+        }
+
+        // POST: products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,productName,category_id,brand_id,quantity,price")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.brand_id = new SelectList(db.brands, "id", "brand1", product.brand_id);
+            ViewBag.category_id = new SelectList(db.categories, "id", "category1", product.category_id);
+            return View(product);
+        }
+
+        // GET: products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            product product = db.products.Find(id);
+            db.products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
